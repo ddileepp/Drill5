662e2226d77409c5a8ee0370292618515891d17b
ddileepp <157563361+ddileepp@users.noreply.github.com>
2026-02-10 18:30:47 +0530

    Refactor names function to use map for full names

--- problem3.js
+++ problem3.js
@@ -1,11 +1,7 @@
-function names(data){
-    const fullNames = []
-
-    for(let i=0;i<data.data.length;i++){
-        let userData = data.data[i];
-        fullNames.push(userData.name[0]+" "+userData.name[1]);
-    }
-
-    return fullNames;
+function names(data) {
+  return data.data.map(function (user) {
+    return user.name.join(" ");
+  });
 }
+
 module.exports = names;
